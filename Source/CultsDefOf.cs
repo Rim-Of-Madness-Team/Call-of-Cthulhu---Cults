aab612d2875775eccd51f343fdf0a9e311f0f357
jecrell <matt.walls31@gmail.com>
2018-09-08 03:30:58 +0900

    Minor update to test IFTTT Discord update code

--- Source/CultsDefOf.cs
+++ Source/CultsDefOf.cs
@@ -245,7 +245,6 @@ namespace CultOfCthulhu
         
         public static MentalStateDef FireStartingSpree;
 
-        public static MentalStateDef WanderConfused;
         public static ResearchProjectDef Forbidden_Reports;
     }
 }
