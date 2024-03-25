6f308f427761304f78a16ceb5de63e1fc1e3c52b
XavierWinters <xavwinters@gmx.co.uk>
2024-03-25 23:44:20 +0000

    Added register all SyncMethod in Multiplayer.cs and referenced Rimworld.MultiplayerAPI in Directory.Build.Props

---
+++ Source/Code/Multiplayer.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using RimWorld;
+using Verse;
+
+namespace CultOfCthulhu;
+
+[StaticConstructorOnStartup]
+internal static class Multiplayer
+{
+    static Multiplayer()
+    {
+        if (!MP.enabled)
+        {
+            return;
+        }
+
+        MP.RegisterAll();
+    }
+    //Remove me before PR. Example of decent syncworker register.
+    //private static void IHaulDestinationWorker(SyncWorker sync, ref IHaulDestination destination)
+    //{
+    //    if (sync.isWriting)
+    //    {
+    //        sync.Write(destination.Map);
+    //        sync.Write(destination.Position);
+    //    }
+    //    else
+    //    {
+    //        var map = sync.Read<Map>();
+    //        var pos = sync.Read<IntVec3>();
+
+    //        destination = map.haulDestinationManager.AllHaulDestinationsListForReading
+    //            .FirstOrDefault(d => d.Position == pos);
+    //    }
+    //}
+}
